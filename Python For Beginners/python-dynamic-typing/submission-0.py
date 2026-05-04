e6b2fc7a2c12c930edfd3914714f3c7036005c6b
Rachel Clement <99040955+rachel-04@users.noreply.github.com>
2026-05-04 11:06:33 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 25
+print(type(variable))
+variable = 25.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Rachu"
+print(type(variable))
+variable = [2,3,4]
+print(type(variable))
