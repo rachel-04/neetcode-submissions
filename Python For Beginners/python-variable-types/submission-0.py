94cc796dd879830eb0a073e0aedcfdea58496934
Rachel Clement <99040955+rachel-04@users.noreply.github.com>
2026-05-04 11:05:25 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 25
+float_type = 45.7
+bool_type = False
+string_type = "Rachu"
+list_type = [1,2,3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
